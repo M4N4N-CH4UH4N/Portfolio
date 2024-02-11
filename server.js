e99af59327021d4bd0dcb2f4a7b4895af9bccfa9
M4N4N-CH4UH4N <mananchauhan6396@gmail.com>
2024-02-11 20:49:41 +0530

    updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 //dotenv configuartion
 dotenv.config();
@@ -11,6 +12,10 @@ const app = express();
 //midlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+});
 
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
